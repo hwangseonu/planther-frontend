2263344a47fda571cdc5e1967e67b6b7eb20c8ce
hwangseonu <hwangseonu12@naver.com>
2018-12-28 00:19:30 +0900

    feat: Add calendar link

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import Navbar from "../../components/Navbar/Navbar";
+import moment from 'moment';
 
 import './Index.css';
 
 class Index extends Component {
 
   render() {
+    const year = parseInt(moment().format('YYYY'));
+    const month = parseInt(moment().format('MM'));
+
     return (
       <div id={'index'}>
         <Navbar/>
@@ -13,11 +17,10 @@ class Index extends Component {
           <div>
             <p className={'page-title'}>Planther</p>
             <p>함께 채워가는 우리만의 달력 "plan together"</p>
-            <button className={'btn-cal'}>Go to calendar</button>
+            <a href={`/calendar/${year}/${month}`}>
+              <button className={'btn-cal'}>Go to calendar</button>
+            </a>
           </div>
-        </div>
-        <div className={'page-section'}>
-
         </div>
       </div>
     )
