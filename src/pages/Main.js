0169d50d013ac621d1ec17ea407eb1887fae5d0a
hwangseonu <hwangseonu12@naver.com>
2019-01-26 23:16:17 +0900

    init: initailization project

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div id={'main'}>
+
+      </div>
+    )
+  }
+}
+
+export default Main;
