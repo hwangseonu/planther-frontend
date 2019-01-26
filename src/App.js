3e2fd25d327a59ec345f81cace09b6969e233a4a
hwangseonu <hwangseonu12@naver.com>
2019-01-26 23:27:33 +0900

    feat: Add main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Main from './pages/Main';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <BrowserRouter>
+          <Fragment>
+            <Route path={'/'} component={Main} exact/>
+          </Fragment>
+        </BrowserRouter>
       </div>
     );
   }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,6 @@ class Main extends Component {
   render() {
     return (
       <div id={'main'}>
-
       </div>
     )
   }
